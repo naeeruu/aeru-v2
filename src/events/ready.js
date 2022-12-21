807af853af5e32c3290a20d73091c792a286e3c7
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-21 16:58:20 +0700

    Create ready.js

---
+++ src/events/ready.js
@@ -0,0 +1,16 @@
+import { Events } from "discord.js";
+
+export default {
+  name: Events.ClientReady,
+  once: true,
+  async execute(client) {
+    await console.log(`Connected! ^^`);
+
+    await client.mongo.connect();
+    await client.mongo.db("client").collection("conn").updateOne({ _id: client.id }, { $set: {timestamp: client.readyTimestamp } }, { upsert: true });
+    setInterval(async () => {
+      const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: client.id });
+      if (timestamp !== client.readyTimestamp) return await process.exit(0);
+    }, 1000)
+  }
+};
