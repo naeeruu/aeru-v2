9a3e0e085ede8d3ceee1d02a44388d7ffa4b63dc
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-21 17:52:06 +0700

    Update interaction.js

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -3,6 +3,7 @@ import { Events } from "discord.js";
 export default {
   name: Events.InteractionCreate,
   async execute(interaction) {
-    await console.log(`Interaction fired!`);
+    const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: interaction.client.id });
+    if (timestamp !== interaction.client.readyTimestamp) return await process.exit(0);
   }
 };
