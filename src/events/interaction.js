7b8f9768f7f9cb335371b5414f6707be5c260116
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-22 02:11:52 +0700

    Update interaction.js

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -7,7 +7,7 @@ export default {
     if (timestamp !== interaction.client.readyTimestamp) return await process.exit(0);
 
     if (interaction.isCommand()) {
-      const command = await interaction.client.commands.find(command => command.data.name === interaction.commandName && command.data.type === (interaction.commandType === undefined ? ApplicationCommandType.ChatInput : interaction.commandType));
+      const command = await interaction.client.commands.find(command => command.data.name === interaction.commandName && command.data.type === (interaction.commandType === ApplicationCommandType.ChatInput ? undefined : interaction.commandType));
       if (command) return await command.execute(interaction);
     }
   }
