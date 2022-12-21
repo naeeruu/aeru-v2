0648af0e8ef074bca99f132a6585237fda5befe1
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-21 17:40:24 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,18 @@ const client = new Client({
     GatewayIntentBits.Guilds
   ]
 });
+
+client.mongo = new MongoClient(process.env.MONGO_URI);
 client.commands = new Collection();
 
 import * as events from "./events/events.js";
-console.log(events);
+for (const eventParam of Object.keys(events)) {
+  const event = events[eventParam];
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+};
+
+client.login(process.env.DISCORD_TOKEN);
