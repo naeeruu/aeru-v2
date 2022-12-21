ae13802160d31d19797f942aa54cafc8994d7767
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-21 20:52:41 +0700

    Update interaction.js

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -6,7 +6,9 @@ export default {
     const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: interaction.client.id });
     if (timestamp !== interaction.client.readyTimestamp) return await process.exit(0);
 
-    const command = await client.commands.find(command => command.data.name === interaction.commandName && command.data.type === [undefined, ApplicationCommandType.User, ApplicationCommandType.Message]);
-    if (command) return await command.execute(interaction);
+    if (interaction.isCommand()) {
+      const command = await client.commands.find(command => command.data.name === interaction.commandName && command.data.type === [undefined, ApplicationCommandType.User, ApplicationCommandType.Message]);
+      if (command) return await command.execute(interaction);
+    }
   }
 };
