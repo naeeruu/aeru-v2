ac7d2de952c7c4dcaaf84f4db93fe6e209b0d99a
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-22 22:51:00 +0700

    Create messageCreate.js

---
+++ src/events/messageCreate.js
@@ -0,0 +1,11 @@
+import { Events } from "discord.js";
+
+export default {
+  name: Events.MessageCreate,
+  async execute(message) {
+    if (message.author.bot) return;
+
+    const { timestamp } = await message.client.mongo.db("client").collection("conn").findOne({ _id: message.client.id });
+    if (timestamp !== message.client.readyTimestamp) return await process.exit(0);
+  }
+};
