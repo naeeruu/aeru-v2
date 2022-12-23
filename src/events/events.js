5d60baf76dedc067aedcdb6bc269e7ca0d666039
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-23 09:13:30 +0700

    Update events.js

--- src/events/events.js
+++ src/events/events.js
@@ -1,2 +1,2 @@
-export { default as InteractionCreate } from "./interaction.js";
+export { default as MessageCreate } from "./messageCreate.js";
 export { default as ClientReady } from "./ready.js";
