936a0b2ac24b7d8c64668f44359e7e604ab4d772
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-23 15:30:17 +0700

    Update events.js

--- src/events/events.js
+++ src/events/events.js
@@ -1,3 +1,3 @@
-export { default as interactionCreate } from "./interaction.js";
-export { default as messageCreate } from "./messageCreate.js";
-export { default as ready } from "./ready.js";
+export { default as InteractionCreate } from "./interaction.js";
+export { default as MessageCreate } from "./messageCreate.js";
+export { default as ClientReady } from "./ready.js";
