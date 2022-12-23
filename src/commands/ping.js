35d2dea2f028e439feee0ef9e1dd96cc4c7cd243
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-23 14:06:39 +0700

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,7 +3,8 @@ import { SlashCommandBuilder } from "discord.js";
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Tes sinyal🌂"),
+    .setDescription("🐨 — Tes sinyal(*´ω｀*)")
+    .setDMPermission(false),
   async execute(interaction) {
     await interaction.reply({
       content: `Ehe! (≡^∇^≡) ${Date.now() - interaction.createdTimestamp}ms`
