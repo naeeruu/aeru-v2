4fb54bf720d0123cc6d5abc6e0b25f94bb345f34
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-23 08:32:17 +0700

    Update interaction.js

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -1,14 +1,11 @@
-import { ApplicationCommandType, Events } from "discord.js";
+import { ApplicationCommandType } from "discord.js";
 
-export default {
-  name: Events.InteractionCreate,
-  async execute(interaction) {
-    const { timestamp } = await interaction.client.mongo.db("client").collection("conn").findOne({ _id: interaction.client.id });
-    if (timestamp !== interaction.client.readyTimestamp) return await process.exit(0);
+export default async execute(interaction) {
+  const { timestamp } = await interaction.client.mongo.db("client").collection("conn").findOne({ _id: interaction.client.id });
+  if (timestamp !== interaction.client.readyTimestamp) return await process.exit(0);
 
-    if (interaction.isCommand()) {
-      const command = await interaction.client.commands.find(command => command.data.name === interaction.commandName && command.data.type === (interaction.commandType === ApplicationCommandType.ChatInput ? undefined : interaction.commandType));
-      if (command) return await command.execute(interaction);
-    }
+  if (interaction.isCommand()) {
+    const command = await interaction.client.commands.find(command => command.data.name === interaction.commandName && command.data.type === (interaction.commandType === ApplicationCommandType.ChatInput ? undefined : interaction.commandType));
+    if (command) return await command.execute(interaction);
   }
 };
