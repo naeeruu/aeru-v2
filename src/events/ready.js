f04cc4efa8424b658f3c14381d1f371441d78ccc
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-23 15:03:19 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,12 +2,15 @@ export default {
   once: true,
   async execute(client) {
     await console.log(`Connected! ^^`);
+    await client.application.commands.set(client.commands.map(command => command.data));
 
     await client.mongo.connect();
     await client.mongo.db("client").collection("conn").updateOne({ _id: client.id }, { $set: {timestamp: client.readyTimestamp } }, { upsert: true });
     setInterval(async () => {
       const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: client.id });
       if (timestamp !== client.readyTimestamp) return await process.exit(0);
-    }, 1000)
+    }, 1000);
+
+    await client.application.fetch();
   }
 };
