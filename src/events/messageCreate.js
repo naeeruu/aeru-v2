ec64f2bee56b92c052c0e09f708b461852520391
vennfx <120244424+vennfx@users.noreply.github.com>
2022-12-23 11:13:28 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,5 @@
 import { Events } from "discord.js";
-import { defaultPrefix } from "../config.json" assert { type: "json" };
+import config from "../config.json" assert { type: "json" };
 
 export default {
   async execute(message) {
