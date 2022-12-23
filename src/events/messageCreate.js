70b606b526146d8e17bf45c79806edb175b79f00
vennfx <120244424+vennfx@users.noreply.github.com>
2022-12-23 11:16:13 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,13 +7,5 @@ export default {
 
     const { timestamp } = await message.client.mongo.db("client").collection("conn").findOne({ _id: message.client.id });
     if (timestamp !== message.client.readyTimestamp) return await process.exit(0);
-
-    const prefix = (await message.client.mongo.db("prefix").collection(message.guildId).findOne({ _id: message.guildId }))?.data ?? defaultPrefix;
-    if (message.content.startsWith(prefix)) {
-      const args = message.content.slice(prefix.length).trim().split(/ +/g);
-      const command = await client.commands.find(command => command.data.name === args.shift());
-
-      if (command) return await command.execute({ args, command, message, prefix });
-    }
   }
 };
