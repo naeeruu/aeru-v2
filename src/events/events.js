daa8fc914e7bd08d439591666e4f1d1e94468627
nyv <120244424+imnyv@users.noreply.github.com>
2022-12-23 08:26:59 +0700

    Update events.js

--- src/events/events.js
+++ src/events/events.js
@@ -1,2 +1,4 @@
-export { default as InteractionCreate } from "./interaction.js";
-export { default as ClientReady } from "./ready.js";
+import { Events } from "discord.js";
+
+export { default as Events.InteractionCreate } from "./interaction.js";
+export { default as Events.ClientReady } from "./ready.js";
