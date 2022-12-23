8101e38ba0371be6fdf41cabc385c94a6d8c73b6
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-23 15:41:13 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,5 +4,15 @@ export default {
 
     const { timestamp } = await message.client.mongo.db("client").collection("conn").findOne({ _id: message.client.id });
     if (timestamp !== message.client.readyTimestamp) return await process.exit(0);
+
+    try {
+    const autores = await message.client.mongo.db("autoresponse").collection(message.guildId).find().toArray().find(data => data.tag.toLowerCase() === message.content.toLowerCase());
+      if (autores) await message.reply({
+        content: autores.response,
+        allowedMentions: { parse: [] }
+      });
+    } catch(error) {
+      console.error(error);
+    }
   }
 };
