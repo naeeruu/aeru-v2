a1f458ff5a50c6ed07061df63dfb172a49300c00
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-23 20:54:13 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -6,7 +6,7 @@ export default {
     if (timestamp !== message.client.readyTimestamp) return await process.exit(0);
 
     try {
-    const autores = await message.client.mongo.db("autoresponse").collection(message.guildId).find().toArray().find(data => data.tag.toLowerCase() === message.content.toLowerCase());
+    const autores = (await message.client.mongo.db("autoresponse").collection(message.guildId).find().toArray()).find(data => data.tag.toLowerCase() === message.content.toLowerCase());
       if (autores) await message.reply({
         content: autores.response,
         allowedMentions: { parse: [] }
