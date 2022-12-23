0651195b3888eb1396eade5dc3432387f1448773
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-23 14:48:59 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 export const discord = {
-  token: process.env.DISCORD_TOKEN,
+  moderators: [
+    "937998965754986526"
+  ],
   embed: {
     color: 0xFFEAE0
-  }
+  },
+  token: process.env.DISCORD_TOKEN
 };
 export const mongo = {
   uri: process.env.MONGO_URI
