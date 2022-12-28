f248102d9a433d3f1723beb75dd466e78271c1f4
vnnfx <120244424+vnnfx@users.noreply.github.com>
2022-12-28 17:02:26 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,8 +2,8 @@ export default {
   class: "DiscordClient",
   once: true,
   async execute(client) {
-    await console.log(`Connected! ^^`);
-    await client.application.commands.set(client.commands.map(command => command.data));
+    console.log(`Connected! ^^`);
+    await client.application.fetch();
 
     await client.mongo.connect();
     await client.mongo.db("client").collection("conn").updateOne({ _id: client.id }, { $set: {timestamp: client.readyTimestamp } }, { upsert: true });
@@ -11,7 +11,5 @@ export default {
       const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: client.id });
       if (timestamp !== client.readyTimestamp) return await process.exit(0);
     }, 1000);
-
-    await client.application.fetch();
   }
 };
